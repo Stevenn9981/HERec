943040f3cb415f99c746cfa206264ad60be4c2b4
Stevenn9981 <stevenn9981@gmail.com>
2021-05-03 15:05:00 +0800

    modify

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-nohup python HERec_spl.py --um +ubu --im +bub > ubu+bub.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcibu --im +bub > ubcibu+bub.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcabu --im +bub > ubcabu+bub.txt 2>&1 &
-nohup python HERec_spl.py --um +ucou --im +bub > ubcabu+bub.txt 2>&1 &
-nohup python HERec_spl.py --um +ubu --im +bcib > ubu+bcib.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcibu --im +bcib > ubcibu+bcib.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcabu --im +bcib > ubcabu+bcib.txt 2>&1 &
-nohup python HERec_spl.py --um +ucou --im +bcib > ubcabu+bcib.txt 2>&1 &
-nohup python HERec_spl.py --um +ubu --im +bcab > ubu+bcab.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcibu --im +bcab > ubcibu+bcab.txt 2>&1 &
-nohup python HERec_spl.py --um +ubcabu --im +bcab > ubcabu+bcab.txt 2>&1 &
-nohup python HERec_spl.py --um +ucou --im +bcab > ubcabu+bcab.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ubu --im +bub > ubu+bub.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ubcibu --im +bub > ubcibu+bub.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ubcabu --im +bub > ubcabu+bub.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ucou --im +bub > ubcabu+bub.txt 2>&1 &
+python -u HERec_spl.py --um +ubu --im +bcib > ubu+bcib.txt
+nohup python -u HERec_spl.py --um +ubcibu --im +bcib > ubcibu+bcib.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ubcabu --im +bcib > ubcabu+bcib.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ucou --im +bcib > ubcabu+bcib.txt 2>&1 &
+python -u HERec_spl.py --um +ubu --im +bcab > ubu+bcab.txt
+nohup python -u HERec_spl.py --um +ubcibu --im +bcab > ubcibu+bcab.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ubcabu --im +bcab > ubcabu+bcab.txt 2>&1 &
+nohup python -u HERec_spl.py --um +ucou --im +bcab > ubcabu+bcab.txt 2>&1 &
